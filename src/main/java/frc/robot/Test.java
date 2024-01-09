cb465d979bd55eff2fa12bbb4a324f319190ed76
Benjamin Kelemen <71470711+BenKelemen@users.noreply.github.com>
2024-01-09 11:04:37 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/Test.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Robot extends TimedRobot {
+
+  @Override
+  public void robotInit() {}
+  
+  @Override
+  public void teleopPeriodic() {}
+  
+  @Override
+  public void autonomousInit() {}
+}
